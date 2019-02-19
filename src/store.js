ef52a4d0416e641c605db49f851d534a6f3d67c5
Brian De Maio <bdemaio1@gmail.com>
2019-02-19 12:01:16 -0500

    Setting store actions and mutations for clearing user on logout, and for the future posts functionality

--- src/store.js
+++ src/store.js
@@ -17,6 +17,12 @@ export const store = new Vuex.Store({
         .catch(err => {
           console.log(err);
         });
+    },
+    clearData({ commit }) {
+      commit("setCurrentUser", null);
+      commit("setUserProfile", {});
+      commit("setPosts", null);
+      commit("setHiddenPosts", null);
     }
   },
   mutations: {
@@ -25,6 +31,23 @@ export const store = new Vuex.Store({
     },
     setUserProfile(state, val) {
       state.userProfile = val;
+    },
+    setPosts(state, val) {
+      if (val) {
+        state.posts = val;
+      } else {
+        state.posts = [];
+      }
+    },
+    setHiddenPosts(state, val) {
+      if (val) {
+        // make sure not to add duplicates
+        if (!state.hiddenPosts.some(x => x.id === val.id)) {
+          state.hiddenPosts.unshift(val);
+        }
+      } else {
+        state.hiddenPosts = [];
+      }
     }
   }
 });
