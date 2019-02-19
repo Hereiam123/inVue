6d464a038e7d92bbacf7d20f4f14c558dec1f952
Brian De Maio <bdemaio1@gmail.com>
2019-02-19 08:51:21 -0500

    Adding firebase to project, with authentication and firestore

---
+++ firebaseConfig.js
@@ -0,0 +1,41 @@
+import firebase from "firebase";
+import "firebase/firestore";
+import firebaseKeys from "./firebaseKeys";
+
+// firebase init goes here
+const config = {
+  apiKey: firebaseKeys.apiKey,
+  authDomain: firebaseKeys.authDomain,
+  databaseURL: firebaseKeys.databaseURL,
+  projectId: firebaseKeys.projectId,
+  storageBucket: firebaseKeys.storageBucket,
+  messagingSenderId: firebaseKeys.messagingSenderId
+};
+firebase.initializeApp(config);
+
+// firebase utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+const currentUser = auth.currentUser;
+
+// date issue fix according to firebase
+const settings = {
+  timestampsInSnapshots: true
+};
+db.settings(settings);
+
+// firebase collections
+const usersCollection = db.collection("users");
+const postsCollection = db.collection("posts");
+const commentsCollection = db.collection("comments");
+const likesCollection = db.collection("likes");
+
+export {
+  db,
+  auth,
+  currentUser,
+  usersCollection,
+  postsCollection,
+  commentsCollection,
+  likesCollection
+};
