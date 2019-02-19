26c317f85e9d6fefa4b15604f394f2e2da0d88b0
Brian De Maio <bdemaio1@gmail.com>
2019-02-19 08:55:50 -0500

    Moved firebase config file to src folder

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import { store } from "./store.js";
+const fb = require("./firebaseConfig.js");
 
 Vue.config.productionTip = false;
 
